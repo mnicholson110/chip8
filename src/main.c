90eae0dbe5582ab84165b6f64d5f795c4477aeb5
matt <mnicholson110@gmail.com>
2025-01-03 08:22:57 -0600

    font and vm setup

--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_keycode.h>
 #include <stdbool.h>
 
 typedef enum {
@@ -10,66 +8,138 @@ typedef enum {
 } vm_state_t;
 
 typedef struct {
+    vm_state_t state;
+    uint8_t ram[4096];
+    // todo: make these pointers into ram
+    bool display[64 * 32];
+    uint16_t stack[12];
+    uint8_t V[16];
+    uint16_t I;
+    uint16_t PC;
+    uint8_t delay_time;
+    uint8_t sound_timer;
+    bool keyboard[16];
+    char* rom_name;
+} vm_t;
+
+typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
-    vm_state_t vm_state;
-} state_t;
+} program_t;
 
-int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
-    state_t state = {0};
-    // init SDL systems
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
-        SDL_Log("Unable to init SDL: %s\n", SDL_GetError());
-        return 1;
+bool init_vm(vm_t* vm, char* rom_name) {
+    const uint8_t font[] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0,  // 0
+        0x20, 0x60, 0x20, 0x20, 0x70,  // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0,  // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0,  // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10,  // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0,  // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0,  // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40,  // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0,  // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0,  // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90,  // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0,  // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0,  // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0,  // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0,  // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80,  // F
+    };
+
+    memcpy(&vm->ram[0], font, sizeof(font));
+
+    FILE* rom = fopen(rom_name, "rb");
+    if (!rom) {
+        SDL_Log("Unable to open rom: %s\n", rom_name);
+        return false;
     }
 
-    // init window and renderer
-    state.window = SDL_CreateWindow(
+    fseek(rom, 0, SEEK_END);
+    const long rom_size = ftell(rom);
+    const long max_size = sizeof(vm->ram) - 0x200;
+    rewind(rom);
+
+    if (rom_size > max_size) {
+        SDL_Log("Rom file is too big: %s\n", rom_name);
+        return false;
+    }
+
+    if (fread(&vm->ram[0x200], rom_size, 1, rom) != 1) {
+        SDL_Log("Unable to read rom file: %s\n", rom_name);
+        return false;
+    };
+
+    fclose(rom);
+
+    vm->state = RUNNING;
+    vm->PC = 0x200;
+    vm->rom_name = rom_name;
+
+    return true;
+}
+
+bool init_sdl(program_t* program) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+        SDL_Log("Unable to init SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    program->window = SDL_CreateWindow(
         "Chip8",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1280,  // * 20
-        640,   // * 20
+        1280,  // 64 * 20
+        640,   // 32 * 20
         0);
 
-    if (!state.window) {
+    if (!program->window) {
         SDL_Log("Unable to create SDL window: %s", SDL_GetError());
         return 1;
     }
 
-    state.renderer = SDL_CreateRenderer(
-        state.window,
+    program->renderer = SDL_CreateRenderer(
+        program->window,
         -1,
         SDL_RENDERER_ACCELERATED);
 
-    if (!state.renderer) {
+    if (!program->renderer) {
         SDL_Log("Unable to create SDL renderer: %s", SDL_GetError());
-        return 1;
+        return false;
     }
+    return true;
+}
 
-    state.vm_state = RUNNING;
+void cleanup(program_t* program) {
+    SDL_DestroyRenderer(program->renderer);
+    SDL_DestroyWindow(program->window);
+    SDL_Quit();
+}
 
-    SDL_SetRenderDrawColor(state.renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(state.renderer);
+int main(int argc, char** argv) {
+    (void)argc;
+    program_t program = {};
+    init_sdl(&program);
+
+    vm_t vm = {};
+    init_vm(&vm, argv[1]);
 
     // main loop
-    while (state.vm_state != QUIT) {
+    while (vm.state != QUIT) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym) {
                         case SDLK_ESCAPE:
-                            state.vm_state = QUIT;
+                            vm.state = QUIT;
                             break;
                         default:
                             break;
                     }
                     break;
                 case SDL_QUIT:
-                    state.vm_state = QUIT;
+                    vm.state = QUIT;
                     break;
                 default:
                     break;
@@ -78,11 +148,11 @@ int main(int argc, char** argv) {
 
         SDL_Delay(16);
 
-        SDL_RenderPresent(state.renderer);
+        SDL_SetRenderDrawColor(program.renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(program.renderer);
+        SDL_RenderPresent(program.renderer);
     }
 
-    SDL_DestroyRenderer(state.renderer);
-    SDL_DestroyWindow(state.window);
-    SDL_Quit();
+    cleanup(&program);
     return 0;
 }
